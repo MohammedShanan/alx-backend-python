4d28ca628e48e0104125e6c9f86f085a8378b08b
mohammedshanan <mohammedshana12@gmail.com>
2024-11-03 19:20:58 +0200

    added  More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -53,42 +53,55 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_public_repos(self, mock_get_json: MagicMock) -> None:
         """Tests the `public_repos` method."""
         test_payload = {
-            "repos_url": "https://api.github.com/users/google/repos",
-            "repos": [
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
                 {
-                    "id": 100,
-                    "name": "Test",
+                    "id": 7697149,
+                    "name": "episodes.dart",
                     "private": False,
                     "owner": {
                         "login": "google",
-                        "id": 200,
+                        "id": 1342004,
                     },
                     "fork": False,
-                    "url": "https://api.github.com/repos/google/Test",
-                    "created_at": "2024-04-19T00:31:37Z",
-                    "updated_at": "2024-11-23T11:53:58Z",
+                    "url": "https://api.github.com/repos/google/episodes.dart",
+                    "created_at": "2013-01-19T00:31:37Z",
+                    "updated_at": "2019-09-23T11:53:58Z",
                     "has_issues": True,
-                    "forks": 17,
+                    "forks": 22,
                     "default_branch": "master",
                 },
                 {
-                    "id": 300,
-                    "name": "Tekken",
+                    "id": 8566972,
+                    "name": "kratu",
                     "private": False,
                     "owner": {
                         "login": "google",
-                        "id": 400,
+                        "id": 1342004,
                     },
                     "fork": False,
-                    "url": "https://api.github.com/repos/google/Tekken",
-                    "created_at": "2024-04-19T00:31:37Z",
-                    "updated_at": "2024-11-23T11:53:58Z",
+                    "url": "https://api.github.com/repos/google/kratu",
+                    "created_at": "2013-03-04T22:52:33Z",
+                    "updated_at": "2019-11-15T22:22:16Z",
                     "has_issues": True,
-                    "forks": 18,
+                    "forks": 32,
                     "default_branch": "master",
                 },
-            ],
+            ]
         }
+        mock_get_json.return_value = test_payload["repos"]
+        with patch(
+                "client.GithubOrgClient._public_repos_url",
+                new_callable=PropertyMock,
+                ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(),
+                [
+                    "episodes.dart",
+                    "kratu",
+                ],
+            )
             mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once()
 
